53d01789b6db79c6785f6949812a0ade7f05cd74
chrull <chrull@bergen.org>
2016-08-18 10:41:29 -0400

    Added the EMOJI ME code files
    Added the HTML, CSS, and Javascript. Also added a great job gif photo for the correct window.
    *CSS needs to be updated when the homepage is complete

---
+++ emojimepart/script/emojimove.js
@@ -0,0 +1,58 @@
+
+    
+function smileFunction()
+   { 
+OpenWindow=window.open("", "newwin", "height=250, width=250,toolbar=no,scrollbars="+scroll+",menubar=no");
+OpenWindow.document.write("<TITLE>Smiley Emoji</TITLE>")
+OpenWindow.document.write("<BODY BGCOLOR=#E0EEE0>")
+OpenWindow.document.write("<h1>Hello!</h1>")
+OpenWindow.document.write("That emoji is not surprised. This emoji has a smile on its face, which means it is happy. If someone looks happy, they are likely to be a nice person. Try talking to them! <hr> Exit out of here and try choosing the emoji that is surprised </hr>")
+OpenWindow.document.write("</BODY>")
+OpenWindow.document.write("</HTML>")
+
+OpenWindow.document.close()
+self.name="main"
+   }
+   
+function disappointedFunction()
+   { 
+OpenWindow=window.open("", "newwin", "height=300, width=250,toolbar=no,scrollbars="+scroll+",menubar=no");
+OpenWindow.document.write("<TITLE>Disappointed Emoji</TITLE>")
+OpenWindow.document.write("<BODY BGCOLOR=#E0EEE0>")
+OpenWindow.document.write("<h1>Hello!</h1>")
+OpenWindow.document.write("That emoji is not surprised. This emoji has a sad face and scrunched eyebrows, which means it is disappointed. If someone looks disappointed, something may have happended that they did not want to happen. They may not feel in the mood to talk to you or to anyone, but don't take it personally.<hr> Exit out of here and try choosing the emoji that is surprised </hr>")
+OpenWindow.document.write("</BODY>")
+OpenWindow.document.write("</HTML>")
+
+OpenWindow.document.close()
+self.name="main"
+   }
+   
+function angryFunction()
+   { 
+OpenWindow=window.open("", "newwin", "height=250, width=250,toolbar=no,scrollbars="+scroll+",menubar=no");
+OpenWindow.document.write("<TITLE>Disappointed Emoji</TITLE>")
+OpenWindow.document.write("<BODY BGCOLOR=#E0EEE0>")
+OpenWindow.document.write("<h1>Hello!</h1>")
+OpenWindow.document.write("That emoji is not surprised. This emoji is angry. There are lot of things that make people angry. If someone is yelling or being mean, this is a sign that there are angry. There are other signs that people are angry too.  <hr> Exit out of here and try choosing the emoji that is surprised </hr>")
+OpenWindow.document.write("</BODY>")
+OpenWindow.document.write("</HTML>")
+
+OpenWindow.document.close()
+self.name="main"
+   }
+
+function surpriseFunction()
+   { 
+OpenWindow=window.open("", "newwin", "height=350, width=550,toolbar=no,scrollbars="+scroll+",menubar=no");
+OpenWindow.document.write("<TITLE>Disappointed Emoji</TITLE>")
+OpenWindow.document.write("<BODY BGCOLOR=#E0EEE0>")
+OpenWindow.document.write("<h1>You are right!</h1>")
+OpenWindow.document.writeln("This emoji is surprised! You can tell because their eyebrows are high and their mouth is opened. When someone is surprised, this means that something happened that they did not expect to happen.<hr> <img src ='pictures/greatjob.gif' alt='Good job!'> </hr> <hr>Exit out of here and try the next question. </hr>")
+OpenWindow.document.write("</BODY>")
+OpenWindow.document.write("</HTML>")
+
+OpenWindow.document.close()
+self.name="main"
+   }
+ 
